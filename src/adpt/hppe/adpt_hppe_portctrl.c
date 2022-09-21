226fff6bc422a3716f5666132dd882acbde7981c
zhongjia <quic_zhongjia@quicinc.com>
2022-09-21 22:19:27 +0800

    [qca-ssdk]: fix MHT PHY lost one packets issue
    Change-Id: Idcdc2887912d6cd61f47028c19902e425821d192
    Signed-off-by: zhongjia <quic_zhongjia@quicinc.com>

--- src/adpt/hppe/adpt_hppe_portctrl.c
+++ src/adpt/hppe/adpt_hppe_portctrl.c
@@ -2597,19 +2597,14 @@ _adpt_hppe_port_mux_mac_set(a_uint32_t dev_id, fal_port_t port_id, a_uint32_t po
 static sw_error_t
 adpt_hppe_port_speed_change_mac_reset(a_uint32_t dev_id, a_uint32_t port_id)
 {
-	a_uint32_t uniphy_index = 0, mode = 0;
+	fal_port_interface_mode_t mode = PORT_INTERFACE_MODE_MAX;
 	a_uint32_t rxfc_status = 0, txfc_status = 0;
 	sw_error_t rv = 0;
 
-	if (port_id == HPPE_MUX_PORT1) {
-		uniphy_index = SSDK_UNIPHY_INSTANCE1;
-	} else if (port_id == HPPE_MUX_PORT2) {
-		uniphy_index = SSDK_UNIPHY_INSTANCE2;
-	} else {
-		return SW_OK;
-	}
-	mode = ssdk_dt_global_get_mac_mode(dev_id, uniphy_index);
-	if (mode == PORT_WRAPPER_USXGMII) {
+	rv = adpt_hppe_port_interface_mode_get(dev_id, port_id, &mode);
+	SW_RTN_ON_ERROR(rv);
+	if (mode == PORT_USXGMII || mode == PORT_UQXGMII) {
+		SSDK_DEBUG("xgmac reset for port%d\n", port_id);
 		ssdk_port_mac_clock_reset(dev_id, port_id);
 		/*restore xgmac's pr and pcf setting after reset operation*/
 		rv = adpt_hppe_port_xgmac_promiscuous_mode_set(dev_id,
@@ -5575,14 +5570,24 @@ qca_hppe_mac_sw_sync_task(struct qca_phy_priv *priv)
 					/* configure gcc speed clock according to current speed */
 					adpt_hppe_gcc_port_speed_clock_set(priv->device_id, port_id,
 							phy_status.speed);
-
+					aos_mdelay(100);
 					/* config uniphy speed to usxgmii mode */
 					adpt_hppe_uniphy_speed_set(priv->device_id, port_id,
 							phy_status.speed);
-
+					if(hsl_port_phyid_get(priv->device_id, port_id) ==
+						QCA8084_PHY)
+					{
+						/*do uniphy adpt reset before mac reset*/
+						adpt_hppe_gcc_uniphy_clock_status_set(
+							priv->device_id, port_id, A_TRUE);
+						adpt_hppe_uniphy_port_adapter_reset(priv->device_id,
+							port_id);
+						aos_mdelay(100);
+					}
 					/* reset port mac when speed change under usxgmii mode */
-					adpt_hppe_port_speed_change_mac_reset(priv->device_id, port_id);
-
+					adpt_hppe_port_speed_change_mac_reset(priv->device_id,
+						port_id);
+					aos_mdelay(100);
 					/* config mac speed */
 					adpt_hppe_port_mac_speed_set(priv->device_id, port_id,
 							phy_status.speed);
