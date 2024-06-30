c80eb0f2e39137d81efa21a40d5f2c61e7f5234c
zhongjia <quic_zhongjia@quicinc.com>
2024-06-30 17:35:18 +0800

    [qca-ssdk]: change the log when link-intr-gpio is not defined
    Change-Id: Ifa5360930b74b63ddf237c04631ddbf26e9d86df
    Signed-off-by: zhongjia <quic_zhongjia@quicinc.com>

--- src/init/ssdk_dts.c
+++ src/init/ssdk_dts.c
@@ -905,7 +905,7 @@ ssdk_dt_parse_interrupt(a_uint32_t dev_id, struct device_node *switch_node)
 	if(intr_gpio_num < 0) {
 		intr_gpio_num = of_get_named_gpio(switch_node, "link-intr-gpio", 0);
 		if(intr_gpio_num < 0) {
-			SSDK_INFO("intr-gpio does not exist\n");
+			SSDK_INFO("link-intr-gpio isn’t defined, enabling link polling\n");
 		}
 	}
 	if(intr_gpio_num > 0) {
