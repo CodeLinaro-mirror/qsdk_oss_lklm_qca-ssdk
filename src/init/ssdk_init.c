894779ccc0722f85974b72fbd98e311e961be240
zhongjia <quic_zhongjia@quicinc.com>
2024-08-13 23:59:34 +0800

    [qca-ssdk]: add ipq54xx to of_match_table
    Change-Id: If2517fffd7c4b3a7d9a3b80c64d8d281163077d2
    Signed-off-by: zhongjia <quic_zhongjia@quicinc.com>

--- src/init/ssdk_init.c
+++ src/init/ssdk_init.c
@@ -1918,6 +1918,7 @@ static const struct of_device_id ssdk_of_mtable[] = {
 	{.compatible = "qcom,ess-switch-ipq807x" },
 	{.compatible = "qcom,ess-switch-ipq95xx" },
 	{.compatible = "qcom,ess-switch-ipq53xx" },
+	{.compatible = "qcom,ess-switch-ipq54xx" },
 	{.compatible = "qcom,ess-instance" },
 	{}
 };
