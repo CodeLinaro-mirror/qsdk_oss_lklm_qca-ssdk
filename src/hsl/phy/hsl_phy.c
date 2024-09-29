a6ba8e15014defb36534e160dc5551e16268a9a2
zhongjia <quic_zhongjia@quicinc.com>
2024-09-29 22:43:29 +0800

    [qca-ssdk]: read PHY status manually for mht DSA
    Change-Id: Ie6bd55a1591be8912bb69fac3a184233ac4e9e9b
    Signed-off-by: zhongjia <quic_zhongjia@quicinc.com>

--- src/hsl/phy/hsl_phy.c
+++ src/hsl/phy/hsl_phy.c
@@ -1045,10 +1045,9 @@ hsl_port_phydev_get_status(a_uint32_t dev_id, a_uint32_t port_id,
 	}
 
 	mutex_lock(&phydev->lock);
-	/*when attached_dev is null, the phy state machine is not start,*/
-	/*need to read phy status manully*/
-	if (!phydev->attached_dev)
-		phy_read_status(phydev);
+	/* make sure that ssdk can get the phy realtime status so */
+	/* need to read phy status manually */
+	phy_read_status(phydev);
 	phy_status->link_status = phydev->link;
 	phy_status->speed = phydev->speed;
 	phy_status->duplex = phydev->duplex;
