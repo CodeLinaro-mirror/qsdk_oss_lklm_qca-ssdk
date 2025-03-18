bd6970dc71683f482b62f43c6ede58987a3f0171
zhongjia <quic_zhongjia@quicinc.com>
2025-03-18 19:03:12 +0800

    [qca-ssdk]: remove the unused code of appe l2 vp table
    Change-Id: Id10023d39770726e63ef585c692737157b406fd0
    Signed-off-by: zhongjia <quic_zhongjia@quicinc.com>

--- include/hsl/appe/appe_l2_vp.h
+++ include/hsl/appe/appe_l2_vp.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2021, The Linux Foundation. All rights reserved.
  *
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022, 2025 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -42,151 +42,7 @@ appe_l2_vp_port_tbl_set(
 		a_uint32_t dev_id,
 		a_uint32_t index,
 		union l2_vp_port_tbl_u *value);
-#if 0
-sw_error_t
-appe_l2_vp_port_tbl_vp_type_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
 
-sw_error_t
-appe_l2_vp_port_tbl_vp_type_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_vp_context_active_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_vp_context_active_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_station_move_lrn_en_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_station_move_lrn_en_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_enq_phy_port_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_enq_phy_port_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_policer_index_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_policer_index_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_enq_service_code_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_enq_service_code_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_eg_ctag_fmt_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_eg_ctag_fmt_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_extra_header_len_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_extra_header_len_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_vp_state_check_en_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_vp_state_check_en_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_station_move_fwd_cmd_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_station_move_fwd_cmd_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_isol_profile_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_isol_profile_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_new_addr_fwd_cmd_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_new_addr_fwd_cmd_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-#endif
 sw_error_t
 appe_l2_vp_port_tbl_promisc_en_get(
 		a_uint32_t dev_id,
@@ -198,43 +54,7 @@ appe_l2_vp_port_tbl_promisc_en_set(
 		a_uint32_t dev_id,
 		a_uint32_t index,
 		a_uint32_t value);
-#if 0
-sw_error_t
-appe_l2_vp_port_tbl_new_addr_lrn_en_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
 
-sw_error_t
-appe_l2_vp_port_tbl_new_addr_lrn_en_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_enq_service_code_en_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_enq_service_code_en_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_lrn_lmt_exceed_fwd_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_lrn_lmt_exceed_fwd_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-#endif
 sw_error_t
 appe_l2_vp_port_tbl_port_isolation_bitmap_get(
 		a_uint32_t dev_id,
@@ -246,55 +66,7 @@ appe_l2_vp_port_tbl_port_isolation_bitmap_set(
 		a_uint32_t dev_id,
 		a_uint32_t index,
 		a_uint32_t value);
-#if 0
-sw_error_t
-appe_l2_vp_port_tbl_eg_vlan_fltr_cmd_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
 
-sw_error_t
-appe_l2_vp_port_tbl_eg_vlan_fltr_cmd_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_policer_en_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_policer_en_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_eg_vlan_fmt_valid_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_eg_vlan_fmt_valid_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_vp_eg_data_valid_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_vp_eg_data_valid_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-#endif
 sw_error_t
 appe_l2_vp_port_tbl_dst_info_get(
 		a_uint32_t dev_id,
@@ -306,55 +78,7 @@ appe_l2_vp_port_tbl_dst_info_set(
 		a_uint32_t dev_id,
 		a_uint32_t index,
 		a_uint32_t value);
-#if 0
-sw_error_t
-appe_l2_vp_port_tbl_lrn_lmt_cnt_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
 
-sw_error_t
-appe_l2_vp_port_tbl_lrn_lmt_cnt_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_lrn_lmt_en_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_lrn_lmt_en_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_isol_en_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_isol_en_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-appe_l2_vp_port_tbl_exception_fmt_ctrl_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_exception_fmt_ctrl_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-#endif
 sw_error_t
 appe_l2_vp_port_tbl_app_ctrl_profile_get(
 		a_uint32_t dev_id,
@@ -366,19 +90,7 @@ appe_l2_vp_port_tbl_app_ctrl_profile_set(
 		a_uint32_t dev_id,
 		a_uint32_t index,
 		a_uint32_t value);
-#if 0
-sw_error_t
-appe_l2_vp_port_tbl_mtu_check_type_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
 
-sw_error_t
-appe_l2_vp_port_tbl_mtu_check_type_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-#endif
 sw_error_t
 appe_l2_vp_port_tbl_invalid_vsi_forwarding_en_get(
 		a_uint32_t dev_id,
@@ -390,19 +102,7 @@ appe_l2_vp_port_tbl_invalid_vsi_forwarding_en_set(
 		a_uint32_t dev_id,
 		a_uint32_t index,
 		a_uint32_t value);
-#if 0
-sw_error_t
-appe_l2_vp_port_tbl_physical_port_mtu_check_en_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
 
-sw_error_t
-appe_l2_vp_port_tbl_physical_port_mtu_check_en_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-#endif
 sw_error_t
 appe_l2_vp_port_tbl_physical_port_get(
 		a_uint32_t dev_id,
@@ -414,17 +114,4 @@ appe_l2_vp_port_tbl_physical_port_set(
 		a_uint32_t dev_id,
 		a_uint32_t index,
 		a_uint32_t value);
-#if 0
-sw_error_t
-appe_l2_vp_port_tbl_eg_stag_fmt_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_l2_vp_port_tbl_eg_stag_fmt_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-#endif
 #endif
