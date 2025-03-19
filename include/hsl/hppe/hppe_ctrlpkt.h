d21c2f2f7836a76730fe9388ed318f4d4c680b56
zhongjia <quic_zhongjia@quicinc.com>
2025-03-19 14:15:16 +0800

    [qca-ssdk] remove the unused code in hppe_ctrlpkt.c
    Change-Id: Ic7781b31170fafdd55941d658156eef602946dd6
    Signed-off-by: zhongjia <quic_zhongjia@quicinc.com>

--- include/hsl/hppe/hppe_ctrlpkt.h
+++ include/hsl/hppe/hppe_ctrlpkt.h
@@ -1,7 +1,7 @@
 /*
  * Copyright (c) 2016-2017, 2021, The Linux Foundation. All rights reserved.
  *
- * Copyright (c) 2022 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022, 2025 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -63,201 +63,13 @@ hppe_ethertype_ctrl_ethertype_set(
 		a_uint32_t dev_id,
 		a_uint32_t index,
 		a_uint32_t value);
-#if 0
-sw_error_t
-hppe_ethertype_ctrl_ethertype_en_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-#endif
+
 sw_error_t
 hppe_ethertype_ctrl_ethertype_en_set(
 		a_uint32_t dev_id,
 		a_uint32_t index,
 		a_uint32_t value);
-#if 0
-sw_error_t
-hppe_app_ctrl_portbitmap_include_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_portbitmap_include_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_cmd_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_cmd_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_portbitmap_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_portbitmap_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_rfdb_index_bitmap_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_rfdb_index_bitmap_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_protocol_bitmap_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_protocol_bitmap_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_in_stg_byp_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_in_stg_byp_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_valid_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_valid_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_l2_sec_byp_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_l2_sec_byp_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_protocol_include_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_protocol_include_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_ethertype_include_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_ethertype_include_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_sg_byp_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_sg_byp_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_rfdb_include_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_rfdb_include_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_ethertype_index_bitmap_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_ethertype_index_bitmap_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-
-sw_error_t
-hppe_app_ctrl_in_vlan_fltr_byp_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-hppe_app_ctrl_in_vlan_fltr_byp_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-#endif
 #ifdef APPE
-#if 0
-sw_error_t
-appe_app_ctrl_port_type_get(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t *value);
-
-sw_error_t
-appe_app_ctrl_port_type_set(
-		a_uint32_t dev_id,
-		a_uint32_t index,
-		a_uint32_t value);
-#endif
 sw_error_t
 appe_l2_cpu_code_ctrl_exception_fmt_ctrl_en_get(
 		a_uint32_t dev_id,
