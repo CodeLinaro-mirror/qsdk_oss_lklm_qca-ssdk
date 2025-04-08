f96487f76bc096250215b88b284ca8c2f0eac1b1
Lei Wei <quic_leiwei@quicinc.com>
2025-04-08 18:01:40 +0800

    qca-ssdk: Enable PCI access in SSDK
    Change-Id: Iada9d41896e6565587e04d22ad94497ecf4954d9
    Signed-off-by: Lei Wei <quic_leiwei@quicinc.com>

--- include/init/ssdk_clk.h
+++ include/init/ssdk_clk.h
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2017, 2019-2020, The Linux Foundation. All rights reserved.
- * Copyright (c) 2022-2024 Qualcomm Innovation Center, Inc. All rights reserved.
+ * Copyright (c) 2022-2025 Qualcomm Innovation Center, Inc. All rights reserved.
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
@@ -342,6 +342,11 @@ qca_gcc_uniphy_port_clock_set(a_uint32_t dev_id, a_uint32_t uniphy_index,
                                 a_uint32_t port_id, a_bool_t enable);
 void ssdk_gcc_clock_init(void);
 void ssdk_gcc_clock_exit(void);
+
+#if IS_ENABLED(CONFIG_QCOM_FPGA_PCI) || defined(SSDK_PCIE_BUS)
+void ssdk_pci_ppe_clock_init(a_uint32_t dev_id);
+#endif
+
 void
 ssdk_port_speed_clock_set(
 	a_uint32_t dev_id,
