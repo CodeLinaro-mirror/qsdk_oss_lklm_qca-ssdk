95affb8e36f14d0c9373634e1bc386bcd48f262f
Feng Pei <fpei@qti.qualcomm.com>
2025-05-12 15:57:21 +0800

    qca-ssdk: Fix the zero value due to the divisibility property
    Change-Id: I05af64701679d4a785e1f9a2b36a91a6807aa1b7
    Signed-off-by: Feng Pei <fpei@qti.qualcomm.com>

--- src/adpt/hppe/adpt_hppe_shaper.c
+++ src/adpt/hppe/adpt_hppe_shaper.c
@@ -535,6 +535,10 @@ __adpt_hppe_shaper_burst_size_to_bucket_size(a_uint32_t burst_size,
 	{
 		temp_bucket_size = ADPT_HPPE_SHAPER_BUCKET_SIZE_MAX;
 	}
+	if(temp_bucket_size == 0)
+	{
+		temp_bucket_size = 1;
+	}
 
 	*bucket_size = temp_bucket_size;
 
